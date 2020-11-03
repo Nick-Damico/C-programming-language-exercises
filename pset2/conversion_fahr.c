49650c5d46a3541e1e7aee6370b6a2ce9b609f99
Nick-Damico <nickalan82@icloud.com>
2020-11-03 07:11:30 -0500

    Feat: Add formatting; Table heading

--- pset2/conversion_fahr.c
+++ pset2/conversion_fahr.c
@@ -5,21 +5,21 @@ int main()
   float cels, fahr;
   int lower, upper, step;
 
-  printf("%s\n","Celsius to Farhenheit conversion");
   /* initialize values */
   lower = 0;
   step = 20;
   upper = 300;
   cels = lower;
-
+  
+  /* Add Table Heading */
+  printf("%s\t%s\n", "Celsius", "Fahrenheit");
+  printf("==================\n");
   while(cels < upper)
   {
     /* convert celsius to farh */
     fahr = (cels * (9.0/5.0)) + 32;
     /* print table */
-    printf("%3.1f\t%6.1f\n", cels, fahr);
+    printf("%7.0f\t%10.1f\n", cels, fahr);
     cels += step;
   }
 }
-
-
