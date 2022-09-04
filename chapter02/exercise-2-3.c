4ca4f8e1c13194483c4d1587b4b5176797926e3d
Nick-Damico <nickalan82@icloud.com>
2022-09-04 17:33:26 -0500

    WIP: Need to fix reference to Math.h pow function

--- chapter02/exercise-2-3.c
+++ chapter02/exercise-2-3.c
@@ -15,26 +15,33 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 #define MAXLENGTH 100
 
-int htoi(char s[]);
-int isHexadecimal(char s[]);
+int htoi(char s[], int len);
+int isHex(char s[]);
 int readline(char s[]);
 
 // TODO: Update to use ARGS once this concept is introduced in the C programming book.
 int main(void) {
+    int len;
     char line[MAXLENGTH];
 
-    while(readline(line) > 0) {
-        printf("check hexadecimal: %d\n", isHexadecimal(line));
-        if (isHexadecimal(line))
-        {
-        }
-        
-        printf("%s - Not a hexadecimal string.\n", line);
+    while((len = readline(line)) > 0) {
+        printf("check hexadecimal: %d\n", isHex(line));
+        if (isHex(line))
+            printf("%d\n", htoi(line, len));
+        else
+            printf("%s - Not a hexadecimal string.\n", line);
     }
+
     return 0;
+}
+
+int isHex(char s[]) {
+    // A valid hexadecimal string can start with Ox or OX.
+    return s[0] == '0' && s[1] == 'x' || s[1] == 'X';
 }
 
 /*
@@ -53,12 +60,17 @@ int main(void) {
 *   We might need to account for the suffix Ox or OX, so
 *   length might be len -2.
 */
-int isHexadecimal(char s[]) {
-    // A valid hexadecimal string can start with Ox or OX.
-    return s[0] == 'o' || s[0] == 'O' && s[1] == 'x' || s[1] == 'X';
-}
+int htoi(char s[], int len) {
+    int i = 2;  // Skip first 2 characters; Delimiter for hexadecimal number ox.
+    int n = 0;
 
-int htoi(char s[]) {
+    while(s[i] != '\n')
+    {
+        // Get the numbers place (length - 1) currentValue of i.
+        n = n + pow(16, (len-1) - i);
+        ++i;
+    }
+    return n;
 }
 
 int readline(char s[]) {
