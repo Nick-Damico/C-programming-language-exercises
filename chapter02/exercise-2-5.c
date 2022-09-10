76198a5880d320c76a7208f1c135028360457215
Nick-Damico <nickalan82@icloud.com>
2022-09-10 10:52:24 -0500

    WIP: Initial upload of exercise-2-5

---
+++ chapter02/exercise-2-5.c
@@ -0,0 +1,32 @@
+/*
+ *  Author:    Nick D'Amico
+ *  Date:      9/10/22
+ *  Exercise:  2-5
+ * 
+ *  Objective:  Write the function any(s1,s2), which returns the first location in the string s1
+ *              where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
+ *              (The standard library function strpbrk does the same job but return a pointer to the location.)
+ * 
+ *  Solution: 
+ *      
+ *  Notes:      
+ * 
+ */
+
+#include <stdio.h>
+
+#define MAXLEN 100
+
+short any(char s1[], char s2[]);
+
+int main(void) {
+    return 0;
+}
+
+short any(char s1[], char s2[])
+{
+    short pos = -1; // Start value in an unfound state.
+
+
+    return pos;
+}
