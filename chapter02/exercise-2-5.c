d1f2422925699cf362439586b57ff13d6d323a36
Nick-Damico <nickalan82@icloud.com>
2022-09-10 11:09:50 -0500

    Maint: Format code

--- chapter02/exercise-2-5.c
+++ chapter02/exercise-2-5.c
@@ -2,15 +2,15 @@
  *  Author:    Nick D'Amico
  *  Date:      9/10/22
  *  Exercise:  2-5
- * 
+ *
  *  Objective:  Write the function any(s1,s2), which returns the first location in the string s1
  *              where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
  *              (The standard library function strpbrk does the same job but return a pointer to the location.)
- * 
- *  Solution: 
- *      
- *  Notes:      
- * 
+ *
+ *  Solution:
+ *
+ *  Notes:
+ *
  */
 
 #include <stdio.h>
@@ -20,7 +20,8 @@
 short any(char s1[], char s2[]);
 void readline(char s[]);
 
-int main(void) {
+int main(void)
+{
     char s1[MAXLEN];
     char s2[MAXLEN];
 
@@ -36,14 +37,12 @@ short any(char s1[], char s2[])
 {
     short pos = -1; // Start value in an unfound state.
 
-    for(int i = 0; i < MAXLEN-1 && s1[i] != '\0' && pos < 0; i++)
-        for (int j = 0; j < MAXLEN-1 && s2[j] != '\0' && pos < 0; i++)
+    for (int i = 0; i < MAXLEN - 1 && s1[i] != '\0' && pos < 0; i++)
+        for (int j = 0; j < MAXLEN - 1 && s2[j] != '\0' && pos < 0; i++)
         {
             if (s1[i] == s2[j])
-               pos = i; 
-            
+                pos = i;
         }
-        
 
     return pos;
 }
@@ -52,8 +51,8 @@ void readline(char s[])
 {
     int c;
     int i = 0;
-    while (i < MAXLEN-1 && (c = getchar()) != EOF && c != '\n')
+    while (i < MAXLEN - 1 && (c = getchar()) != EOF && c != '\n')
         s[i++] = c;
-        
+
     s[i] = '\0';
 }
