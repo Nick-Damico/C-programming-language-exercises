4f0203cac790e77d77ee1848884658180f296e45
Nick-Damico <nickalan82@icloud.com>
2022-09-10 11:05:42 -0500

    Feat: Working solution for exercise 2-5.c

--- chapter02/exercise-2-5.c
+++ chapter02/exercise-2-5.c
@@ -18,8 +18,17 @@
 #define MAXLEN 100
 
 short any(char s1[], char s2[]);
+void readline(char s[]);
 
 int main(void) {
+    char s1[MAXLEN];
+    char s2[MAXLEN];
+
+    readline(s1);
+    readline(s2);
+    short result = any(s1, s2);
+    printf("%d\n", result);
+
     return 0;
 }
 
@@ -27,6 +36,24 @@ short any(char s1[], char s2[])
 {
     short pos = -1; // Start value in an unfound state.
 
+    for(int i = 0; i < MAXLEN-1 && s1[i] != '\0' && pos < 0; i++)
+        for (int j = 0; j < MAXLEN-1 && s2[j] != '\0' && pos < 0; i++)
+        {
+            if (s1[i] == s2[j])
+               pos = i; 
+            
+        }
+        
 
     return pos;
+}
+
+void readline(char s[])
+{
+    int c;
+    int i = 0;
+    while (i < MAXLEN-1 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+        
+    s[i] = '\0';
 }
