30945da5a90247fab5a07ad7a6a9bff5932ee12c
Nick-Damico <nickalan82@icloud.com>
2022-09-14 21:24:04 -0500

    WIP: Additional notes for bitmasking

--- chapter02/exercise-2-6.c
+++ chapter02/exercise-2-6.c
@@ -22,21 +22,32 @@
 #include <stdio.h>
 
 int setbits(int x, int p, int n, int y);
+unsigned getbits(unsigned x, int p, int n);
 
 int main(void)
 {
     /*
     *   Helpful visualization:
     *   x is our target number for setbits and returned value.
-    *   x = 28; 1 1 1 0 0
     *           4 3 2 1 0 ( positions ) 
+    *   x = 28; 1 1 1 0 0
     *             - - -   -> bit to modify are ( 110 )  
+    * 
     *   p is our position indicator.
     *   p = 3; 
     *   
     *   n is number of bits we will be targeting, so p + n will be our bit range.
     *   n = 2;
-    *
+    *   
+    *   x shifted right (p+1-n)
+    *           4 3 2 1 0 ( positions ) 
+    *   x = 24; 1 1 0 0 0 ( starting position)
+    *   x = 07; 0 0 1 1 0 ( after shift )
+    *   ~(~0 << n)1 0 1 1 ( Generate bit mask )
+    *            ---------
+    *             0 0 1 1 ( Result is 3 )     
+    *   
+    * 
     *   y is the source of the updated bit for setting x.
     *   y = 42;  1 0 1 0 1 0
     *            5 4 3 2 1 0 ( positions )
@@ -44,15 +55,21 @@ int main(void)
     *           
     *   setbits(28, 3, 2, 42) == 26(11010)
     */
-    int x = 28;
+    int x = 24;
     int p = 3;
     int n = 2;
-    int y = 42;
-    printf("Result: %d\n", setbits(x,p,n,y));
+    // int y = 42;
+    // printf("Result: %d\n", setbits(x,p,n,y));
+    printf("Result: %d\n", getbits(x,p,n));
     return 0;
 }
 
 int setbits(int x, int p, int n, int y)
 {
     return 0;
+}
+
+unsigned getbits(unsigned x, int p, int n)
+{
+    return ((x >> (p+1-n)) & ~(~0 << n));
 }
