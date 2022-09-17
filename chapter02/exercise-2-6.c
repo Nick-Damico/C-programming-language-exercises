03ee355707d8fdb3da672b6fa95a1249a0f4cd5d
Nick-Damico <nickalan82@icloud.com>
2022-09-17 13:11:26 -0500

    WIP: Get right most n bits of y

--- chapter02/exercise-2-6.c
+++ chapter02/exercise-2-6.c
@@ -47,36 +47,54 @@ int main(void)
     *   ~(~0 << n)1 0 1 1 ( Generate bit mask )
     *            ---------
     *             0 0 1 1 ( Result is 3 )     
+    * 
+    *   we do not care about the n bits of x,
+    *   we want to remove those and replace with y n bits.
+    *   We can x 111 y 010 & == 0 1 0
+    *   We need to flip them to on or 1.
     *   
     * 
     *   Example of setbits:
     *   p = 3, n = 2
+    *   Get y n bits
+    *   -----------------------------------------
     *           7 6 5 4 3 2 1 0 ( positions ) 
     *   y = 7  (0 0 0 0 0 1 1 1) 
-    *                       - -  ( right most bits of y)
-    *                   0 0 0 1  ( ~0 ) 
-    *                   0 1 0 0  ( << left shift ) 
-    *                   1 0 1 1  ( then flip result of shift ) 
+    *                       - -  ( right most n bits of y)
+    *           0 0 0 0 0 0 0 1  ( ~0 ) 
+    *           0 0 0 0 0 1 0 0  ( << left shift n bits (y << n)) 
+    *                   0 0 1 1  ( then flip result of shift ~0) 
     *               &   0 0 1 1  ( bitwise & y == 3 )
     * 
+    *   Get x n bits
+    *   -----------------------------------------
     *           7 6 5 4 3 2 1 0  ( positions ) 
-    *   x = 24 (1 1 1 1 1 1 1 1)
-    *                   -        ( right most bits of y)
-    *                   0 1 0 0  ( right shift ~0 ) 
-    *                   1 0 1 1  ( then flip ~0 ) 
+    *   x = 24 (0 0 0 1 1 0 0 0)
+    *                   - - -    ( find our range )
+    *                   0 0 0 1  ( make mask ~0 ) 
+    *                     1 1 1  ( << left shift p ) 
+                      0 1 1 1 0  ( << push once more ) 
+    *                 0 1 1 1 0  ( bitwise | ) 
+                    0 1 1 0 0 0  ( result would be)
+                0 0 0 1 1 1 1 0  = 30
     */
     int x = 24;
     int p = 3;
     int n = 2;
-    int y = 0;
+    int y = 7;
     printf("Result: %d\n", setbits(x,p,n,y));
-    printf("Result: %d\n", getbits(x,p,n));
+    // printf("Result: %d\n", getbits(x,p,n));
     return 0;
 }
 
 int setbits(int x, int p, int n, int y)
 {
+    //  1. get y n bits.
     return y & ~(~0 << n);
+
+    // get x make mask
+    // return x & (~(~0 << p)) << 1;
+    // return y & ~(~0 << n);
 }
 
 /*
