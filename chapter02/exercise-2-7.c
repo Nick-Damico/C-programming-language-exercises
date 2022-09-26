56a8494e5036a7410dc26cd936c81ba1243b7751
Nick-Damico <nickalan82@icloud.com>
2022-09-25 20:18:36 -0500

    Feat: Completed invert(x,p,n) exercise 2-7

--- chapter02/exercise-2-7.c
+++ chapter02/exercise-2-7.c
@@ -19,6 +19,7 @@ int main(void)
 {
     /*
     *
+    *   Example 1:
     *   x = 30
     *   p = 4
     *   n = 3
@@ -30,12 +31,24 @@ int main(void)
     *   x   ( 0 0 0 0 0 0 1 0 )
     *   result = 2
     * 
+    *   Example 2:
+    *   x = 20
+    *   p = 4
+    *   n = 3
+    *         7 6 5 4 3 2 1 0 
+    *   x   ( 0 0 0 1 0 1 0 0 )
+    *   p           -                   
+    *   n           - - -
+    *       --------------------- ( inverted )
+    *   x   ( 0 0 0 0 1 0 0 0 )
+    *   result = 8
+    * 
     *   Solution 1: Make a mask of 1's (n bits long), ^ XOR against x?
     *       Step 1: Make a mask n bits long of 1's.
     *
     *   1. Make a  
     */
-    unsigned x = 30;
+    unsigned x = 20;
     int p = 4, n = 3;
     
     printf("%d\n", invert(x, p, n));
@@ -45,17 +58,5 @@ int main(void)
 
 unsigned invert(unsigned x, int p, int n)
 {
-    return ~(~0 << n);
-}
-// unsigned setbits(unsigned x, int p, int n, unsigned y)
-// {
-//     return x & ~(~(~0 << n) << (p + 1 - n)) | (y & (~(~0 << n))) << (p + 1 - n);
-// }
-
-/*
- *   Borrowed from C programming book as a test example.
- */
-unsigned getbits(unsigned x, int p, int n)
-{
-    return ((x >> (p + 1 - n)) & ~(~0 << n));
+    return x ^ (~(~0 << n)) << (p + 1 - n);
 }
