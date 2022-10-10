4d35b10d5aa09a5b84c06e3f875581e3762c447e
Nick-Damico <nickalan82@icloud.com>
2022-10-09 20:49:27 -0500

    Initial upload of exercise 2-10.c

---
+++ chapter02/exercise-2-10.c
@@ -0,0 +1,24 @@
+/*
+ *  Author:    Nick D'Amico
+ *  Date:      10/09/22
+ *  Exercise:  2-10
+ *
+ *  Objective:  Rewrite the function lower, which converts upper case letters
+ *              to lower cae, with a conditional expression instead of if-else.
+ *   a = 97
+ *   A = 65
+ * 
+ *   Example 1:
+ *               x = 'B' // 66
+ *               (97 - 65) + 66 = 98
+ *               ('A' - 'a') + x == lower of x.
+ *   
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
