655c00df85a38c9c8fb9e16d292c244cf12346c3
Nick-Damico <nickalan82@icloud.com>
2022-10-12 20:49:14 -0500

    Feat: Complete exercise 2-10.c

--- chapter02/exercise-2-10.c
+++ chapter02/exercise-2-10.c
@@ -5,11 +5,13 @@
  *
  *  Objective:  Rewrite the function lower, which converts upper case letters
  *              to lower cae, with a conditional expression instead of if-else.
- *   a = 97
- *   A = 65
  * 
  *   Example 1:
  *               x = 'B' // 66
+ *               1. Find offset between Lowercase and Uppercase 'A'..'a'
+ *               2. Add that difference to the uppercase Letter to find result.
+ *                  2.b if already, lower return character.
+ * 
  *               (97 - 65) + 66 = 98
  *               ('A' - 'a') + x == lower of x.
  *   
@@ -17,8 +19,16 @@
 
 #include <stdio.h>
 
+int lower(int c);
 int main(void)
 {
-
+    int c;
+    while((c = getchar()) != EOF)
+        putchar(lower(c));
     return 0;
+}
+
+int lower(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? ('a' - 'A') + c : c;
 }
