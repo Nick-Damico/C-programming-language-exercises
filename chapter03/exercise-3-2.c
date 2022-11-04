61a209d5562e130dd9d78751347223725171ce19
Nick-Damico <nickalan82@icloud.com>
2022-11-03 20:55:34 -0500

    Initial upload of exercise 3-2.c

---
+++ chapter03/exercise-3-2.c
@@ -0,0 +1,28 @@
+/*
+ *  Author:    Nick D'Amico
+ *  Date:      11/03/22
+ *  Exercise:  3-2
+ *
+ *  Objective:  Write a function escape(s,t) that coverts characters like newline
+ *              and tab into visible escape sequences like \n and \t as it copies
+ *              the string t to s. Use a switch. Write a function for the other direction as
+ *              well, coverting escape sequences into the real characters.
+ *
+ * 
+ *              
+ */
+
+#include <stdio.h>
+
+// Coverts string t to s replacing newline and tabs with escape characters.
+void escape(char s[], char t[]);
+
+int main(void)
+{
+    return 0; 
+}
+
+void escape(char s[], char t[])
+{
+
+}
