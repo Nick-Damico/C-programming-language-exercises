099e131b23eceb11b798e14eb1cf6c19179bb8a5
Nick-Damico <nickalan82@icloud.com>
2022-11-05 13:38:38 -0500

    Feat: Add readline for capture current line to array

--- chapter03/exercise-3-2.c
+++ chapter03/exercise-3-2.c
@@ -14,15 +14,45 @@
 
 #include <stdio.h>
 
+#define MAX_LEN 100
 // Coverts string t to s replacing newline and tabs with escape characters.
 void escape(char s[], char t[]);
+int readline(char s[]);
 
 int main(void)
 {
+    int i;
+    char s[MAX_LEN];
+    char t[MAX_LEN];
+
+    while ((i = readline(s)) > 0)
+    {
+        printf("length: %i\n", i);
+        // call escape(s, t);
+        // printf("%s\n", t);
+    }
+    
     return 0; 
 }
 
 void escape(char s[], char t[])
 {
+    
+}
 
+int readline(char s[])
+{
+    int c;
+    int i;
+    for (i = 0; i < MAX_LEN - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+
+    if (c == '\n')
+    {
+        s[i] = '\n';
+        ++i;
+    }
+    s[i] = '\0';
+    
+    return i-1; // Do not count the newline character;
 }
